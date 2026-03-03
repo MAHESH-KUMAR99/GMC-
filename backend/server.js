29506efff08f07fe9ab466eef7aed957fdd5af2c
Mahesh <mk766263@gmail.com>
2026-03-03 11:26:08 +0530

    -

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,34 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import cors from 'cors';
+import cors from "cors";
 
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import path from "path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import http from 'http';
-import { Server } from 'socket.io';
-import router from './routes/userRoutes.js';
-import pool from './database.js';
-
-
-
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import http from "http";
+import { Server } from "socket.io";
+import router from "./routes/userRoutes.js";
+import pool from "./database.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 
 // HTTP SERVER FOR SOCKET.IO
 const server = http.createServer(app);
 
 // Initialize Socket.IO
 const io = new Server(server, {
-    cors: {
-        // origin:['https://findpath.co.in','https://www.findpath.co.in'],// Replace with your frontend URL
-        // origin: 'http://192.168.1.11:5173', // Allow your frontend URL
-        origin: ['http://localhost:5173'], // Allow your frontend URL
-        methods: ['GET', 'POST'],
-        credentials: true,
-    },
+  cors: {
+    // origin:['https://findpath.co.in','https://www.findpath.co.in'],// Replace with your frontend URL
+    // origin: 'http://192.168.1.11:5173', // Allow your frontend URL
+    // origin: ["http://localhost:5173"], // Allow your frontend URL
+    origin: true,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
 });
-
 
 // Middleware Setup
 app.use(express.urlencoded({ extended: true }));
@@ -40,41 +36,36 @@ app.use(express.json());
 app.use(cookieParser());
 
 // CORS SETUP
-const corsOptions={
-    // origin:['https://findpath.co.in','https://www.findpath.co.in'],// Replace with your frontend URL
-    // origin: ['http://localhost:8081','exp://192.168.1.8:8081'],// Replace with your frontend URL
-    // origin:'http://192.168.1.11:5173',// Replace with your frontend URL
-    origin:true,
-    credentials:true
-}
+const corsOptions = {
+  // origin:['https://findpath.co.in','https://www.findpath.co.in'],// Replace with your frontend URL
+  // origin: ['http://localhost:8081','exp://192.168.1.8:8081'],// Replace with your frontend URL
+  // origin:'http://192.168.1.11:5173',// Replace with your frontend URL
+  origin: true,
+  credentials: true,
+};
 app.use(cors(corsOptions));
 
-
 // Environment Variables Setup
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 // Template and Urls Setup
 app.use("/req", router);
 // Serve static files from the 'public' folder
-app.use(express.static(path.join(__dirname,"Public")));
+app.use(express.static(path.join(__dirname, "Public")));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, './Public', 'index.html'));
+  res.sendFile(path.join(__dirname, "./Public", "index.html"));
 });
 
 // Database connection
 pool.connect((err, client, release) => {
-    if (err) {
-        return console.log('Error connecting to Neon:', err.stack);
-    }
-    console.log('Connected to Neon PostgreSQL');
-    release();
+  if (err) {
+    return console.log("Error connecting to Neon:", err.stack);
+  }
+  console.log("Connected to Neon PostgreSQL");
+  release();
 });
 
-
-
-
-
 // Start the server
-server.listen(process.env.PORT,'0.0.0.0' ,() => {
-    console.log(`Server running at http://localhost:${process.env.PORT}`);
+server.listen(process.env.PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
 });
